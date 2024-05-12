c645159ca20c31564ddc03d51fc907d1e4b40920
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-05-13 01:34:13 +0530

    fix: simplify spec

--- example.data.ts
+++ example.data.ts
@@ -1,16 +1,16 @@
 import { parseYAML } from "confbox";
 import { readFileSync } from "node:fs";
+import { defineLoader } from "vitepress";
 
 interface Data {
   name: string;
   description: string;
   links: {}[];
 }
-export default {
-  load() {
-    const content = readFileSync("links.yaml", { encoding: "utf8" });
 
-    const data = parseYAML<{ name: string }>(content);
-    return data;
+export default defineLoader({
+  watch: ["./links.yaml"],
+  async load(watchedFiles): Promise<Data> {
+    return parseYAML<Data>(readFileSync(watchedFiles[0], "utf-8"));
   },
-};
+});
--- index.md
+++ index.md
@@ -2,4 +2,10 @@
 import {data} from "./example.data.ts"
 </script>
 
+### {{ data.title }}
+
+{{ data.description }}
+
+<!-- @content -->
+
 <pre>{{ data }}</pre>
